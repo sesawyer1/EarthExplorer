db8235212cfd675d9da86b83d161f28dba422c7d
jll1234 <131935848+jll1234@users.noreply.github.com>
2024-02-05 17:09:37 -0600

    Add files via upload

---
+++ Rocky_Movement.cs
@@ -0,0 +1,66 @@
+
+using UnityEngine;
+
+public class Rocky_Movement : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingRight = false;
+    float jumpPower = 8f;
+    bool isJumping = false;
+    Animator animator;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = 0f;
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalInput = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalInput = -1;
+        }
+
+        FlipSprite();
+
+        if (Input.GetKeyDown(KeyCode.W) && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+            animator.SetBool("isJumping", isJumping);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("xVel", System.Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVel", rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput > 0f || !isFacingRight && horizontalInput < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+        animator.SetBool("isJumping", isJumping);
+    }
+}
